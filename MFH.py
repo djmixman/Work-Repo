fa339a481bbbe31d3a780fd30fecb38f2bcd819f
djmixman <djmixman@gmail.com>
2018-07-07 14:05:52 -0500

    Removed old mfh file and updated .gitignore

--- MFH.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python2
-
-# Setting up external libs
-import logging
-
-# Setting up Logging
-logging.basicConfig(level=logging.INFO)
-log  = logging.getLogger(__name__)
-log.info('Logging facility initalized...')
